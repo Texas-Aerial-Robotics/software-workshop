8573197ac02a6f056d20e3f62b182a31903ff350
Andres Suniaga <andressuniaga.x@gmail.com>
2024-09-22 11:23:35 -0500

    shell script? maybe works

---
+++ initial_setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd ~
+
+#always update/upgrade
+sudo apt update && sudo apt upgrade
+
+# Git (if not already installed
+sudo apt install git
+
+# Python things
+sudo apt install python3
+
+sudo apt install python3-pip
+
+# python packages
+pip3 install numpy
+pip3 install matplotlib
+
+# opencv for python
+pip3 install opencv-contrib-python
+
+# https://github.com/damiafuentes/DJITelloPy 
+pip3 install djitellopy
+
+
